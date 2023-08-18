fca0b592c633c521e59389699f3daecf1c119aba
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-08-18 14:07:16 -0700

    Checking out 4-5 to debug

--- models/note.js
+++ models/note.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
     minlength: 5,
-    required: true
   },
   important: Boolean,
-})
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
